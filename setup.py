2c1bb05ddd349134897599c4e207964301ff5f41
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-03 11:49:52 +0200

    Releasing version 1

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-lightweight-queue',
+    version=1,
     packages=(
         'django_lightweight_queue',
         'django_lightweight_queue.management',
