6aeddf1091956ad2e44c358845fc8c9fd011a01b
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-07 09:46:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-lightweight-queue',
-    version=1,
-    packages=(
-        'django_lightweight_queue',
-        'django_lightweight_queue.management',
-        'django_lightweight_queue.management.commands',
-    )
+
+    url="https://chris-lamb.co.uk/projects/django-lightweight-queue",
+    version='1',
+    description="Lightweight & modular queue and cron system for Django",
+
+    author="Chris Lamb",
+    author_email='chris@chris-lamb.co.uk',
+    license="BSD",
+
+    packages=find_packages(),
 )
